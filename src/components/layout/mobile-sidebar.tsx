00081fc73483d46c14a8851454ac69cd46d8337a
rayhankobir-dev <rayhankobir793@gmail.com>
2024-05-10 17:52:57 +0600

    fix mobile sidebar navigation

--- src/components/layout/mobile-sidebar.tsx
+++ src/components/layout/mobile-sidebar.tsx
@@ -1,11 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import { Home, MenuIcon, ShieldBan } from "lucide-react";
-import { TbSoccerField, TbPlayFootball } from "react-icons/tb";
+import { Home, MenuIcon } from "lucide-react";
 import { useEffect, useState } from "react";
-import { PiSoccerBall } from "react-icons/pi";
 import { cn } from "@/lib/utils";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import Logo from "@/assets/logo.png";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function MobileSidebar() {
@@ -30,10 +29,14 @@ export function MobileSidebar() {
         </SheetTrigger>
         <SheetContent side="right" className="w-full !px-0">
           <div className="-translate-y-14 h-screen overflow-hidden overflow-y-scroll space-y-4 py-8 ">
-            <div className="w-full h-14 inline-flex items-center gap-2 border-b px-4 text-green-600">
-              <PiSoccerBall size={25} />
-              <p className="italic font-semibold text-lg">Soccer</p>
-            </div>
+            <Link
+              to="/"
+              onClick={() => setOpen(false)}
+              className="w-full h-14 inline-flex items-center gap-2 border-b px-4 text-orange-600"
+            >
+              <img src={Logo} alt="Travella" />
+              <p className="italic font-semibold text-lg">Travella</p>
+            </Link>
             <div className="py-2">
               <div className="space-y-1">
                 <NavLink
@@ -44,8 +47,8 @@ export function MobileSidebar() {
                   }}
                   className={({ isActive }) =>
                     cn(
-                      "group flex items-center rounded-md mx-2 px-3 py-2.5 text-sm font-medium hover:text-green-600",
-                      isActive ? "text-green-700" : "text-black"
+                      "group flex items-center rounded-md mx-2 px-3 py-2.5 text-sm font-medium hover:text-orange-600",
+                      isActive ? "text-orange-700" : "text-black"
                     )
                   }
                 >
@@ -55,65 +58,49 @@ export function MobileSidebar() {
 
                 <NavLink
                   end={true}
-                  to="/videos/genre/mid-filder"
+                  to="/privacy-policy"
                   onClick={() => {
                     setOpen(false);
                   }}
                   className={({ isActive }) =>
                     cn(
-                      "group flex items-center rounded-md mx-2 px-3 py-2.5 text-sm font-medium hover:text-green-600",
-                      isActive ? "text-green-700" : "text-black"
+                      "group flex items-center rounded-md mx-2 px-3 py-2.5 text-sm font-medium hover:text-orange-600",
+                      isActive ? "text-orange-700" : "text-black"
                     )
                   }
                 >
-                  <TbSoccerField className="mr-2 h-4 w-4" />
-                  <span>Mid Filder</span>
+                  <span>Privacy & Policy</span>
                 </NavLink>
 
                 <NavLink
                   end={true}
-                  to="/videos/genre/defense"
+                  to="/faq"
                   onClick={() => {
                     setOpen(false);
                   }}
                   className={({ isActive }) =>
                     cn(
-                      "group flex items-center rounded-md mx-2 px-3 py-2.5 text-sm font-medium hover:text-green-600",
-                      isActive ? "text-green-700" : "text-black"
+                      "group flex items-center rounded-md mx-2 px-3 py-2.5 text-sm font-medium hover:text-orange-600",
+                      isActive ? "text-orange-700" : "text-black"
                     )
                   }
                 >
-                  <ShieldBan className="mr-2 h-4 w-4" />
-                  <span>Difense</span>
-                </NavLink>
-
-                <NavLink
-                  to="/videos/genre/striker"
-                  onClick={() => {
-                    setOpen(false);
-                  }}
-                  className={({ isActive }) =>
-                    cn(
-                      "group flex items-center rounded-md mx-2 px-3 py-2.5 text-sm font-medium hover:text-green-600",
-                      isActive ? "text-green-700" : "text-black"
-                    )
-                  }
-                >
-                  <TbPlayFootball className="mr-2 h-4 w-4" />
-                  <span>Striker</span>
+                  <span>FAQ's</span>
                 </NavLink>
 
                 <div className="flex gap-1.5 px-4">
                   <NavLink
+                    onClick={() => setOpen(false)}
                     to="/login"
-                    className="w-full md:w-fit px-10 py-3  text-orange-500 rounded-full border border-orange-500 text-center duration-500"
+                    className="w-full md:w-fit px-10 py-2  text-orange-500 rounded-full border border-orange-500 text-center duration-500"
                   >
                     Login
                   </NavLink>
 
                   <NavLink
+                    onClick={() => setOpen(false)}
                     to="/login"
-                    className="w-full md:w-fit px-10 py-3 bg-orange-600 hover:bg-orange-500 rounded-full text-white text-center duration-500"
+                    className="w-full md:w-fit px-10 py-2 bg-orange-600 hover:bg-orange-500 rounded-full text-white text-center duration-500"
                   >
                     Login
                   </NavLink>
