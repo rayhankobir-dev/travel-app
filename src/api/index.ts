0afd1beae0d3933b5d9a66b4d1b2e06f5bb22746
rayhankobir-dev <rayhankobir793@gmail.com>
2024-06-11 00:08:27 +0600

    fix: endpoint updated

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 // src/api/axios.js
 import axios from "axios";
-const baseURL = import.meta.env.VITE_API_URL;
+export const baseURL = import.meta.env.VITE_API_URL;
 
 // Public Axios instance
 export const publicAxios = axios.create({ baseURL });
